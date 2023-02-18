4b265653545171c6ee1cd38eb6322b2eaa21c5a6
AinurSafin <krlai@yandex.ru>
2023-02-18 08:53:56 +0300

    add uppercase_first

--- str_func.py
+++ str_func.py
@@ -2,3 +2,8 @@ def capital_letters():
     letters = input()
     return letters.upper()
     '''Функция которая возвращает все заглавные буквы'''
+
+def uppercase_first():
+    first_letters = input()
+    return first_letters.title()
+    '''Функция которая возвращает заглавные первые буквы'''
